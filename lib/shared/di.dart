43f810ceddba0a1459103704df9ea65defbfbd3c
siposgyula <g.sipos@upscale-labs.com>
2026-05-08 12:02:54 +0200

    jumped over to Token from Type as key to allow multiple DI entries of the same type

--- lib/shared/di.dart
+++ lib/shared/di.dart
@@ -3,76 +3,82 @@
 
 class DI {
   //This can hold one factory per type. Good for now, could be expanded if we want to incorporate ProviderFamily-like functionality
-  static final Map<Type, Factory> _factories = {};
+  static final Map<Token, Factory> _factories = {};
 
   //For the actual instances
-  static final Map<Type, dynamic> _container = {};
+  static final Map<Token, dynamic> _container = {};
 
   //If some class updates, we want the dependents to update as well. This can track dependencies.
-  static final Map<Type, Set<Type>> _dependents = {};
+  static final Map<Token, Set<Token>> _dependents = {};
 
-  static void register<T>(Factory<T> factory) {
-    _factories[T] = factory;
-    //If an older version of the same type is already live, get rid of those
-    if (_container.containsKey(T)) {
-      _invalidate(T);
-    }
+  static Token<T> register<T>(Factory<T> factory) {
+    final token = Token<T>._();
+    _factories[token] = factory;
+    return token;
   }
 
   //We track what type the [get] was called on, so with other [get] calls during the initial get we can autimatically track dependencies
-  static Type? _typeCurrentlyGetting;
+  static Token? _tokenCurrentlyGetting;
   //Track build types to prevent circular dependencies
-  static final Set<Type> _circuitBreaker = {};
+  static final Set<Token> _circuitBreaker = {};
 
   //dependets tracking relies on this being a sync call. Update dependents tracking first if you want this to be async (but you shouldn't want)
-  static T get<T>() {
-    if (_container.containsKey(T)) {
-      return _container[T];
+  static T get<T>(Token<T> token) {
+    if (_container.containsKey(token)) {
+      return _container[token];
     }
 
-    final wasNotAlreadyDependent = _circuitBreaker.add(T);
+    final wasNotAlreadyDependent = _circuitBreaker.add(token);
     if (!wasNotAlreadyDependent) {
       throw CircularDependencyException(
-        'Exception: $T is trying to depend on itself via ${_circuitBreaker.join(' -> ')} -> $T',
+        'Exception: $T is trying to depend on itself via ${_circuitBreaker.map((token) => token.type).join(' -> ')} -> $T',
       );
     }
 
     //if this [get] is called during a [get] call, we know that the type of the original call is depending on this type
-    if (_typeCurrentlyGetting != null && _typeCurrentlyGetting != T) {
+    if (_tokenCurrentlyGetting != null && _tokenCurrentlyGetting != token) {
       //if nothing is depending on T yet, we create the set
-      _dependents[T] ??= {};
-      _dependents[T]!.add(_typeCurrentlyGetting!);
+      _dependents[token] ??= {};
+      _dependents[token]!.add(_tokenCurrentlyGetting!);
     }
 
-    final factory = _factories[T];
+    final factory = _factories[token];
     if (factory == null) {
       throw MissingFactoryException("Forgot to register factory for $T");
     } else {
-      final previousBuilding = _typeCurrentlyGetting;
-      _typeCurrentlyGetting = T;
+      final previousBuilding = _tokenCurrentlyGetting;
+      _tokenCurrentlyGetting = token;
       try {
         final instance = factory();
-        _container[T] = instance;
+        _container[token] = instance;
         return instance as T;
       } finally {
-        _typeCurrentlyGetting = previousBuilding;
+        _tokenCurrentlyGetting = previousBuilding;
         //By removing only this value (and not clearing everything), we circumvent false positive circular errors in sibling dependencies
-        _circuitBreaker.remove(T);
+        _circuitBreaker.remove(token);
       }
     }
   }
 
-  static void _invalidate(Type type) {
-    final dependents = _dependents[type];
+  static void invalidate(Token token) {
+    final dependents = _dependents[token];
     if (dependents != null) {
       //deep copy the list to prevent concurrent modification
       for (final dependency in List.from(dependents)) {
-        _invalidate(dependency);
+        invalidate(dependency);
       }
     }
-    _container.remove(type);
-    _dependents.remove(type);
+    _container.remove(token);
+    _dependents.remove(token);
   }
+}
+
+//lightweight class used as a locator for providers
+final class Token<T> {
+  //creates a new object every time
+  Token._();
+
+  Type get type => T;
 }
 
 // since everything is static in the DI, no need to pass it in here like usual, factories just can DI.get() themselves
